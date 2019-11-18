947209f51c41383dcb61feaa8b8940f2495d9f50
xhq <pxohlqo@foxmail.com>
2019-11-18 09:57:47 +0800

    update

--- algplayground/src/main/java/me/pxohlqo/algplayground/view/playGround/PlayGroundAty.kt
+++ algplayground/src/main/java/me/pxohlqo/algplayground/view/playGround/PlayGroundAty.kt
@@ -2,6 +2,7 @@ package me.pxohlqo.algplayground.view.playGround
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import me.pxohlqo.algplayground.R
 
 class PlayGroundAty : AppCompatActivity() {
@@ -9,5 +10,6 @@ class PlayGroundAty : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_ground_aty)
+
     }
 }
