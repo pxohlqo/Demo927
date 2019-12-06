c5cc29dbcae70970dd81637b661396a216ceb834
pxohlqo <pxohlqo@foxmail.com>
2019-12-06 18:23:01 +0800

    Update  —default message see details in readme.md

--- algplayground/src/main/java/me/pxohlqo/algplayground/model/algorithmAndDataStructrue/CPriorityQueue.java
+++ algplayground/src/main/java/me/pxohlqo/algplayground/model/algorithmAndDataStructrue/CPriorityQueue.java
@@ -1,5 +1,7 @@
 package me.pxohlqo.algplayground.model.algorithmAndDataStructrue;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -64,5 +66,117 @@ public class CPriorityQueue<T extends Comparable<T>> {
     // Removes the root of the heap, O(log(n))
     public T poll() {
         return removeAt(0);
+    }
+
+    public boolean contains(T elem) {
+        for (int i = 0; i < heapSize; i++) {
+            if (heap.get(i).equals(elem)) return true;
+        }
+        return false;
+    }
+
+    public void add(T elem) {
+        if (elem == null) throw new IllegalArgumentException();
+
+        if (heapSize < heapCapity) {
+            heap.set(heapSize, elem);
+        } else {
+            heap.add(elem);
+            heapCapity++;
+        }
+
+        swim(heapSize);
+        heapSize++;
+    }
+
+    // Tests if the value of node i <= node j
+    // This method assumes i & j are valid indices, O(1)
+    private boolean less(int i, int j) {
+        T node1 = heap.get(i);
+        T node2 = heap.get(j);
+        return node1.compareTo(node2) <= 0;
+    }
+
+    // Perform bottom up node swim, O(log(n))
+    private void swim(int k) {
+        int parent = (k - 1) / 2;
+        while (k > 0 && less(k, parent)) {
+            swap(parent, k);
+            k = parent;
+
+            parent = (k - 1) / 2;
+        }
+    }
+
+    // Top down node sink, O(log(n))
+    private void sink(int k) {
+        while (true) {
+            int left = 2 * k + 1;
+            int right = 2 * k + 2;
+            int smallest = left;
+            if (right < heapSize && less(right, left)) smallest = right;
+
+            // Stop if we're outside the bounds of the tree
+            // or stop early if we cannot sink k anymore
+            if (left >= heapSize || less(k, smallest)) break;
+
+            // Move down the tree following the smallest node
+            swap(smallest, k);
+            k = smallest;
+        }
+    }
+
+    private void swap(int i, int j) {
+        T elem_i = heap.get(i);
+        T elem_j = heap.get(j);
+
+        heap.set(i, elem_j);
+        heap.set(j, elem_i);
+    }
+
+    public boolean remove(T element) {
+        if (element == null) return false;
+        for (int i = 0; i < heapSize; i++) {
+            if (element.equals(heap.get(i))) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private T removeAt(int i) {
+        if (isEmpty()) return null;
+
+        heapSize--;
+        T removed_data = heap.get(i);
+        swap(i, heapSize);
+        heap.set(heapSize, null);
+        if (i == heapSize) return removed_data;
+        T elem = heap.get(i);
+        sink(i);
+        if (heap.get(i).equals(elem)) swim(i);
+        return removed_data;
+    }
+
+    private boolean isMinHeap(int k) {
+        if (k >= heapSize) return true;
+        int left = k * 2 + 1;
+        int right = k * 2 + 2;
+
+        // Make sure that the current node k is less than
+        // both of its children left, and right if they exist
+        // return false otherwise to indicate an invalid heap
+        if (left < heapSize && !less(k, left)) return false;
+        if (right < heapSize && !less(k, right)) return false;
+
+        // Recurse on both children to make sure they're also valid heaps
+        return isMinHeap(left) && isMinHeap(right);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return heap.toString();
     }
 }
